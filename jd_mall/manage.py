98a75c26e265f84e9f8e2d722f12b42e5ace64f2
fytang <1467827322@qq.com>
2020-07-14 17:47:41 +0800

    重新指定配置文件

--- jd_mall/manage.py
+++ jd_mall/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jd_mall.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jd_mall.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
